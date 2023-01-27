433fdf5d4165531506401bf813dde685fa68cdaf
TheZacher5645 <zander5645@yahoo.com>
2023-01-26 22:31:42 -0600

    created all parse functions (untested)

--- Blokus/board history.hh
+++ Blokus/board history.hh
@@ -14,13 +14,18 @@ namespace /*private*/ {
 		PieceID id;
 		unsigned x, y;
 	};
+
+	using numPlayers_t  = unsigned;
+	using playerOrder_t = std::vector<unsigned>;
+	using move_t        = std::variant<PlayerMove, NoMove>;
+	using movesList_t   = std::vector<move_t>;
 }
 
 struct BoardHistory {
-	unsigned numPlayers;
-	std::vector<unsigned> playerOrder;
+	numPlayers_t numPlayers;
+	playerOrder_t playerOrder;
 	// player color is not needed by the computer
-	std::vector<std::variant<PlayerMove,NoMove>>;
+	movesList_t movesList;
 };
 
 
@@ -40,52 +45,185 @@ namespace /*private*/ {
 
 	constexpr isWhitespace(char c) { return Is_Either(c,' ',',','\r','\n','\t'); }
 
+	// forward declare all parse functions, so we can write them
+	// in any order we want
+	template <typename T> using parseFunc    = std::optional<T>/**/(std::stringstream&);
+	template <typename T> using parseFuncEOF = std::optional<T>/**/(std::stringstream&,auto&&);
+	/*                 */ using parseFuncNoReturn =        bool/**/(std::stringstream&);
+
+	parseFuncEOF<BoardHistory>  parse_CANONICAL_FORMAT;
+	parseFunc<playerOrder_t>    parse_PLAYER_ORDER;
+	parseFuncNoReturn           parse_COLOR_DATA;
+	parseFunc<PlayerMove>       parse_PIECE_POS;
+	parseFunc<PieceID>          parse_ID;
+	parseFunc<unsigned>         parse_INT;
+	parseFuncNoReturn           parse_whitespace;
+
 	// if we fail, move the curor back to the start
-	#define returnFail str.seekg(result.first ); return std::nullopt;
+	#define returnFail str.seekg(range.first); return {}; // nullopt, or false
 	// otherwise, set the cursor back 1 position
-	#define returnPass str.seekg(result.second); return result;
+	#define returnPass str.seekg(range.second); return result;
 
-	// assets/canonical parse diagram.png
-	using parseFunc = std::optional<ssRange>(std::stringstream&);
-	parseFunc parse_CANNONICAL_FORMAT, parse_INT;
+	// nextEOF is a predicate functor which says if the current char is the last char
+	std::optional<BoardHistory> parse_CANONICAL_FORMAT(std::stringstream& str, auto&& lastChar) {
+		BoardHistory result = {};
+		ssRange range = {str.tellg(), str.tellg()};
+		enum { NUM, ws1, ORDER, ws2, COLOR, ws3, MOVE, ws4 } state = NUM;
+		for (char c; str.get(c); range.second += std::streamoff{1}) {
+			if (state == NUM) {
+				auto numPlayers = parse_INT(str);
+				if (!numPlayers) { returnFail; }
+				result.numPlayers = numPlayers;
+				state = ws1;
+			} else if (state == ws1) {
+				if (!parse_whitespace(str)) { returnFail; }
+				state = ORDER;
+			} else if (state == ORDER) {
+				auto playerOrder = parse_PLAYER_ORDER(str);
+				if (!playerOrder) { returnFail; }
+				result.playerOrder = playerOrder;
+				if (lastChar()) { break; }
+				state = ws2;
+			} else if (state == ws2) {
+				if (!parse_whitespace(str)) { returnFail; }
+				if (str.peek() == 'c') { state = COLOR; }
+				else { state = MOVE; }
+			} else if (state == COLOR) {
+				if (!parse_COLOR_DATA(str)) { returnFail; }
+				if (lastChar()) { break; }
+				state = ws3;
+			} else if (state == ws3) {
+				if (!parse_whitespace(str)) { returnFail; }
+				state = MOVE;
+			} else if (state == MOVE) {
+				if (c == 'x') {
+					result.movesList.push_back(NoMove{});
+				} else {
+					auto move = parse_PIECE_POS(str);
+					if (!move) { returnFail; }
+					result.movesList.push_back(move);
+				}
+				if (lastChar()) { break; }
+				state = ws4;
+			} else if (state == ws4) {
+				if (!parse_whitespace(str)) { returnFail; }
+				state = MOVE;
+			}
+		}
+		returnPass;
+	}
 
+	std::optional<playerOrder_t> parse_PLAYER_ORDER(std::stringstream& str) {
+		playerOrder_t result = {};
+		ssRange range = {str.tellg(), str.tellg()};
+		enum { FIRST, REST } state = FIRST;
+		for (char c; str.get(c); range.second += std::streamoff{1}) {
+			if (state == FIRST) {
+				if (!('0' <= c&&c <= '9')) { returnFail; }
+				result.push_back(c-'0');
+				state = REST;
+			} else if (state == REST) {
+				if (!('0' <= c&&c <= '9')) { break; }
+				result.push_back(c-'0');
+			}
+		}
+		returnPass;
+	}
 
-	std::optional<ssRange> parseID(std::stringstream& str) {
-		ssRange result = {str.tellg(), str.tellg()};
-		enum { X, POINT, Y } state = X;
-		for (char c; str.get(c); result.second += std::streamoff{1}) {
-			if (state == X) {
-				if(!parseINT(str)) { returnFail; }
-				state = POINT;
-			} else if (state == POINT) {
-				if (c != '.') { returnFail; }
+	bool parse_COLOR_DATA(std::stringstream& str) {
+		bool result = true;
+		ssRange range = {str.tellg(), str.tellg()};
+		enum { TAG, FIRST, REST } state = FIRST;
+		for (char c; str.get(c); range.second += std::streamoff{1}) {
+			if (state == TAG) {
+				if (!(c == 'c' && str.get() == ':')) { returnFail; }
+				state = FIRST;
+			} else if (state == FIRST) {
+				if (!(('a' <= c&&c <= 'z') || ('A' <= c&&c <= 'Z'))) { returnFail; }
+				state = REST;
+			} else if (state == REST) {
+				if (!(('a' <= c&&c <= 'z') || ('A' <= c&&c <= 'Z'))) { break; }
+			}
+		}
+		returnPass;
+	}
+
+	std::optional<PlayerMove> parse_PIECE_POS(std::stringstream& str) {
+		PlayerMove result = {};
+		ssRange range = {str.tellg(), str.tellg()};
+		enum { ID, ws1, X, ws2, Y} state = ID;
+		for (char c; str.get(c); range.second += std::streamoff{1}) {
+			if (state == ID) {
+				auto id = parse_ID(str);
+				if (!id) { returnFail; }
+				result.id = id;
+				state = ws1;
+			} else if (state == ws1) {
+				if (!parse_whitespace(str)) { returnFail; }
+				state = X;
+			} else if (state == X) {
+				auto x = parse_INT(str);
+				if (!x) { returnFail; }
+				result.x = x;
+				state = ws2;
+			} else if (state == ws2) {
+				if (!parse_whitespace(str)) { returnFail; }
 				state = Y;
 			} else if (state == Y) {
-				if (!parseINT(str)) { returnFail; }
+				auto y = parse_INT(str);
+				if (!y) { returnFail; }
+				result.y = y;
 				break;
 			}
 		}
 		returnPass;
 	}
 
-	std::optional<ssRange> parseINT(std::stringstream& str) {
-		ssRange result = {str.tellg(), str.tellg()};
-		enum { FIRST, REST } state = FIRST;
-		for (char c; str.get(c); result.second += std::streamoff{1}) {
-			if (state == FIRST) {
-				if (!('0' <= c&&c <= '9')) { returnFail; }
-				state = REST;
-			} else if (state == REST) {
-				if (!('0' <= c&&c <= '9')) { break; }
+	std::optional<PieceID> parse_ID(std::stringstream& str) {
+		PieceID result = {0,0};
+		ssRange range = {str.tellg(), str.tellg()};
+		enum { NUM, POINT, ROT } state = NUM;
+		for (char c; str.get(c); range.second += std::streamoff{1}) {
+			if (state == NUM) {
+				auto num = parse_INT(str);
+				if(!num) { returnFail; }
+				result.num = num;
+				state = POINT;
+			} else if (state == POINT) {
+				if (c != '.') { returnFail; }
+				state = ROT;
+			} else if (state == ROT) {
+				auto rot = parse_INT(str);
+				if (!rot) { returnFail; }
+				result.rot = rot;
+				break;
 			}
 		}
 		returnPass;
 	}
 
-	std::optional<ssRange> parseWhitespace(std::stringstream& str) {
-		ssRange result = {str.tellg(), str.tellg()};
+	std::optional<unsigned> parse_INT(std::stringstream& str) {
+		unsigned result = 0;
+		ssRange range = {str.tellg(), str.tellg()};
 		enum { FIRST, REST } state = FIRST;
-		for (char c; str.get(c); result.second += std::streamoff{1}) {
+		for (char c; str.get(c); range.second += std::streamoff{1}) {
+			if (state == FIRST) {
+				if (!('0' <= c&&c <= '9')) { returnFail; }
+				result = c-'0';
+				state = REST;
+			} else if (state == REST) {
+				if (!('0' <= c&&c <= '9')) { break; }
+				result = 10*result + (c-'0')
+			}
+		}
+		returnPass;
+	}
+
+	bool parse_whitespace(std::stringstream& str) {
+		bool result = true;
+		ssRange range = {str.tellg(), str.tellg()};
+		enum { FIRST, REST } state = FIRST;
+		for (char c; str.get(c); range.second += std::streamoff{1}) {
 			if (state == FIRST) {
 				if (!isWhitespace(c)) { returnFail; }
 				state = REST;
@@ -103,7 +241,8 @@ namespace /*private*/ {
 enum class fileFormat { CANONICAL };
 
 template <fileFormat format = fileFormat::CANONICAL>
-BoardHistory ParseHistory(std::stringstream& strRaw) {
+// TODO: change strRaw back into a istringstream
+BoardHistory readGame(std::stringstream& strRaw) {
 	std::cout << "Parsing History...\n";
 	/* preprocess */
 	// Create 'str' variable wich is just strRaw without comments or leading/trailing whitespaces
@@ -157,23 +296,7 @@ BoardHistory ParseHistory(std::stringstream& strRaw) {
 
 	std::cout << "\"" << str.str() << "\"\n";
 
-	// /* tokenize */
-	// // (I'm actually not too familair with interpreter/compiler terms,
-	// // so tokenize might not be the right name...)
-	// enum dataType {
-	// 	PLAYER_COUNT ,
-	// 	PLYER ORDER ,
-	// 	COLOR_DATA ,
-	// 	PIECE_DATA
-	// };
-	// std::vector<std::pair<dataType,std::string_view>> tokens {};
-
-	// enum {
-
-	// } state;
-
-	// /* parse pre-game options */
-
-	BoardHistory board{};
-	return board;
+	return parse_CANONICAL_FORMAT(str, [&] { return str.tellg() == strRage.second; });
 }
+
+void writeGame(BoardHistory&, std::ostringstream& str);
